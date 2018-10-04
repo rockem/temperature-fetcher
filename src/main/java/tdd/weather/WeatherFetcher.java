b0b9ace11ac3b23daa91421584aa0d8721a8868a
Eli Segal <esegal@heed-global.com>
2018-10-04 18:39:39 +0300

    in progress

---
+++ src/main/java/tdd/weather/WeatherFetcher.java
@@ -0,0 +1,10 @@
+package tdd.weather;
+
+public class WeatherFetcher {
+    public void fetchFor(String city) {
+
+    }
+
+    public static class UnknownCityException extends RuntimeException {
+    }
+}
---
+++ src/test/java/test/tdd/weather/WeatherMapMock.java
@@ -0,0 +1,7 @@
+package test.tdd.weather;
+
+public class WeatherMapMock {
+    public void shouldRetrieve(String city) {
+
+    }
+}
